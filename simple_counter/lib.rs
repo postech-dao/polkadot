63959130bd3cdf95fedf945d3fa416a5648f2cff
chemonoworld <jinwoo0451@naver.com>
2022-07-20 18:10:25 +0900

    style: Modify comments and args according to the code convention

--- simple_counter/lib.rs
+++ simple_counter/lib.rs
@@ -5,20 +5,19 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod counter {
-
     use ink_prelude::vec::Vec;
-
     use ink_storage::traits::SpreadAllocate;
 
     #[ink(storage)]
     #[derive(SpreadAllocate)]
     pub struct State {
         count: u64,
-        auth: Vec<AccountId>, //We could use Mapping<,> of course.
+        auth: Vec<AccountId>,
         init: bool,
     }
 
-    #[ink(event)] //Event emitted when transaction occurs
+    /// Event emitted when transaction occurs
+    #[ink(event)]
     pub struct Transaction {
         value: u64,
     }
@@ -35,27 +34,30 @@ mod counter {
     pub type Result<T> = core::result::Result<T, Error>;
 
     impl State {
+        /// Creates a new simple_counter contract.
         #[ink(constructor)]
-        pub fn new(_initCount: u64) -> Self {
+        pub fn new(init_count: u64) -> Self {
             Self {
-                count: _initCount,
+                count: init_count,
                 auth: Vec::new(),
                 init: false,
             }
         }
 
+        /// Initialize the contract.
         #[ink(message)]
-        pub fn init(&mut self, _initCount: u64, _auth: AccountId) {
+        pub fn init(&mut self, init_count: u64, _auth: AccountId) {
             if self.init {
                 panic!("Already initalized");
             }
 
-            self.count = _initCount;
+            self.count = init_count;
             self.auth.push(_auth);
             self.init = true;
         }
 
-        #[ink(message)] //Check the caller is in auth
+        /// Checks the caller is in auth
+        #[ink(message)]
         pub fn only_auth(&self) {
             let from = Self::env().caller();
 
@@ -68,7 +70,8 @@ mod counter {
             }
         }
 
-        #[ink(message)] //Execute our transaction
+        /// Executes tx
+        #[ink(message)]
         pub fn execute(&mut self, input: u64) -> Result<u64> {
             self.only_auth();
 
@@ -83,7 +86,8 @@ mod counter {
             Ok(input)
         }
 
-        #[ink(message)] //Add auth
+        /// Registers an auth
+        #[ink(message)]
         pub fn add_auth(&mut self, new_auth: AccountId) -> Result<()> {
             self.only_auth();
 
@@ -96,7 +100,8 @@ mod counter {
             Ok(())
         }
 
-        #[ink(message)] //Remove auth from Vec<AccountId>
+        /// Removes an auth from Vec<AccountId>
+        #[ink(message)]
         pub fn remove_auth(&mut self, _auth: AccountId) -> Result<()> {
             self.only_auth();
 
@@ -113,54 +118,55 @@ mod counter {
             Ok(())
         }
 
+        /// Increases the count by one.
         #[ink(message)]
         pub fn increment(&mut self) {
             self.only_auth();
             self.count += 1;
         }
 
+        /// Decreases the count by one.
         #[ink(message)]
         pub fn decrement(&mut self) {
             self.only_auth();
             self.count -= 1;
         }
 
+        /// Resets the count.
         #[ink(message)]
         pub fn reset(&mut self) {
             self.only_auth();
             self.count = 0;
         }
 
-        #[ink(message)] //Since below 2 functions are "view" function, we don't need to check whether caller is in auth or not.
+        /// Returns the count.
+        #[ink(message)]
         pub fn get_count(&self) -> u64 {
             self.count
         }
 
+        /// Returns the list of auths.
         #[ink(message)]
         pub fn get_auth(&self) -> Vec<AccountId> {
             self.auth.clone()
         }
     }
 
-    //Below test cases are based on above functions.
-    //We can check that our functions work well.
-
+    /// Unit tests
     #[cfg(test)]
     mod tests {
         use super::*;
         use crate::counter::Error::*;
-
         use ink_lang as ink;
 
         #[ink::test]
         fn new_works() {
             let state = State::new(0);
 
-            assert_eq!(state.count, 0); //count should be 0
-            assert_eq!(state.auth.len(), 0); //We don't push any auth yet
+            assert_eq!(state.count, 0);
+            assert_eq!(state.auth.len(), 0);
         }
 
-        //Do init
         #[ink::test]
         fn init_works() {
             let mut state = State::new(0);
@@ -170,7 +176,6 @@ mod counter {
             assert_eq!(state.auth[0], AccountId::from([0x01; 32]));
         }
 
-        //Below case should panic
         #[ink::test]
         #[should_panic]
         fn prevent_re_init_works() {
@@ -181,7 +186,6 @@ mod counter {
             state.init(3, AccountId::from([0x01; 32])); //panic occurs
         }
 
-        //Get count
         #[ink::test]
         fn get_count_works() {
             let state = State::new(0);
@@ -189,7 +193,6 @@ mod counter {
             assert_eq!(state.get_count(), 0);
         }
 
-        //Get Auth
         #[ink::test]
         fn get_auth_works() {
             let mut state = State::new(0);
@@ -227,7 +230,7 @@ mod counter {
             assert_eq!(result2, Ok(()));
         }
 
-        //Testing increment, decrement, reset in one test
+        // Testing increment, decrement, reset in one test
         #[ink::test]
         fn count_control_works() {
             let mut state = State::new(0);
@@ -249,7 +252,7 @@ mod counter {
             assert_eq!(state.count, 0);
         }
 
-        //Testing transaction
+        // Testing transaction
         #[ink::test]
         fn execute_works() {
             let mut state = State::new(0);
