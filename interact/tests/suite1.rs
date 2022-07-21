d21345b3cf5133727a6dd9658b8f59997889cb92
Lewis <kimhyunwoo554@gmail.com>
2022-07-21 20:02:57 +0900

    feat: implement interaction and add polkadot metadata.scale file for using subxt

--- interact/tests/suite1.rs
+++ interact/tests/suite1.rs
@@ -1,59 +1,62 @@
+use std::str::FromStr;
+
 use serde::{Deserialize, Serialize};
 
+use sp_core::crypto;
 use sp_keyring::AccountKeyring;
 use subxt::{
     rpc::Subscription,
     sp_runtime::{generic::Header, traits::BlakeTwo256},
-    ClientBuilder, DefaultConfig, PolkadotExtrinsicParams,
+    ClientBuilder, DefaultConfig, PairSigner, PolkadotExtrinsicParams,
 };
 
-#[subxt::subxt(runtime_metadata_path = "../../../metadata.scale")]
+#[subxt::subxt(runtime_metadata_path = "../artifacts/polkadot_metadata.scale")]
 pub mod polkadot {}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
-    full_node_url: String,
+    test_local_node_url: String,
+    test_node_url: String,
     account_public: String,
-    account_private: String,
-    wasm_binary_path: String,
+    account_password: String,
+    account_seed: String,
     // and so on
 }
 
 impl Config {
     pub fn read_from_env() -> Self {
         serde_json::from_str(
-            &std::fs::read_to_string(
-                std::env::var("TEST_CONFIG")
-                    .expect("Environment variable for the config file path is missing"),
-            )
-            .expect("Failed to locate the config file"),
+            &std::fs::read_to_string("./test_config_example.json")
+                .expect("Environment variable for the config file path is missing"),
         )
         .expect("Failed to parse the config")
     }
 }
 
 #[tokio::test]
-#[ignore]
 async fn check_connection() {
     tracing_subscriber::fmt::init();
 
+    let config = Config::read_from_env();
+
     let api = ClientBuilder::new()
-        .set_url("wss://rococo-contracts-rpc.polkadot.io:443")
+        .set_url(&config.test_node_url)
         .build()
         .await
         .unwrap()
         .to_runtime_api::<polkadot::RuntimeApi<DefaultConfig, PolkadotExtrinsicParams<DefaultConfig>>>();
 
-    let _block_hash = api.client.rpc().block_hash(None).await.unwrap();
+    let block_hash = api.client.rpc().block_hash(None).await;
+
+    assert!(block_hash.is_ok());
 }
 
 #[tokio::test]
-#[ignore]
 async fn check_block_number() {
-    tracing_subscriber::fmt::init();
+    let config = Config::read_from_env();
 
     let api = ClientBuilder::new()
-        .set_url("wss://rococo-contracts-rpc.polkadot.io:443")
+        .set_url(&config.test_node_url)
         .build()
         .await
         .unwrap()
@@ -62,49 +65,60 @@ async fn check_block_number() {
     let mut blocks: Subscription<Header<u32, BlakeTwo256>> =
         api.client.rpc().subscribe_finalized_blocks().await.unwrap();
 
-    let mut first_block_number: u32 = 0;
-    let mut second_block_number: u32 = 0;
-    let mut count = 0;
+    let first_block = blocks.next().await.unwrap().expect("failed to read block");
 
-    while count != 2 {
-        let block = if let Some(Ok(block)) = blocks.next().await {
-            block
-        } else {
-            todo!()
-        };
-        if first_block_number == 0_u32 {
-            first_block_number = block.number;
-        } else {
-            second_block_number = block.number;
-        }
-        count += 1;
-    }
-    assert!(second_block_number > first_block_number);
+    let second_block = blocks.next().await.unwrap().expect("failed to read block");
+
+    assert!(second_block.number > first_block.number);
 }
 
+/// Below test will fail since Account 'alice' doesn't have any balance.
+/// It will work on local node or any accounts that have balance.
 #[tokio::test]
 #[ignore]
 async fn check_account() {
-    tracing_subscriber::fmt::init();
+    let config = Config::read_from_env();
 
-    let alice = AccountKeyring::Alice.to_account_id();
+    let user = crypto::AccountId32::from_str(&config.account_public).unwrap();
 
     let api = ClientBuilder::new()
-        .set_url("wss://rococo-contracts-rpc.polkadot.io:443")
+        .set_url(&config.test_node_url)
         .build()
         .await
         .unwrap()
         .to_runtime_api::<polkadot::RuntimeApi<DefaultConfig, PolkadotExtrinsicParams<DefaultConfig>>>();
 
-    let account = api.storage().system().account(&alice, None).await.unwrap();
+    let account = api.storage().system().account(&user, None).await.unwrap();
 
-    assert!(account.data.free == account.data.fee_frozen); //Don't have any balances to pay gas fee
+    // account.data.free should be larger than account.data.fee_frozen to pay gas fee
+    assert!(account.data.free >= account.data.fee_frozen);
 }
 
+/// Below test will fail since Account 'alice' doesn't have balance.
+/// It will work on local node or any accounts that have balance.
 #[tokio::test]
 #[ignore]
-async fn upload_modify_and_query() {
-    let _config = Config::read_from_env();
-    // upload the contract, submit a transaction that modifies its state, and query the state
-    unimplemented!();
+async fn transfer() {
+    let config = Config::read_from_env();
+
+    let signer = PairSigner::new(AccountKeyring::Alice.pair());
+
+    let api = ClientBuilder::new()
+        .set_url(&config.test_node_url)
+        .build()
+        .await
+        .unwrap()
+        .to_runtime_api::<polkadot::RuntimeApi<DefaultConfig, PolkadotExtrinsicParams<DefaultConfig>>>();
+
+    let dest = AccountKeyring::Bob.to_account_id().into();
+
+    let tx_hash = api
+        .tx()
+        .balances()
+        .transfer(dest, 123_456_789)
+        .unwrap()
+        .sign_and_submit_then_watch_default(&signer)
+        .await;
+
+    assert!(tx_hash.is_ok());
 }
