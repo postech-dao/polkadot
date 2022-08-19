2967eff85dfc37d970547f20865b9bd2e74f82a6
hyeonLewis <kimhyunwoo554@gmail.com>
2022-08-20 01:00:07 +0900

    feat: implement deno http server

---
+++ deno-http-server/abi.ts
@@ -0,0 +1,7 @@
+export const ABI_PATH = {
+    SIMPLE_COUNTER: "./contracts/simple_counter.contract",
+    LIGHT_CLIENT: "./contracts/light_client.contract",
+    TREASURY: "./contracts/treasury",
+  } as const;
+  
+  export type ABI_PATH_OPTIONS = typeof ABI_PATH[keyof typeof ABI_PATH];
