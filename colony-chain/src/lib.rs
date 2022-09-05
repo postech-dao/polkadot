cb91fc04bb69bfb1b96d13f251782565d25a2a01
chemonoworld <jinwoo0451@naver.com>
2022-09-06 00:55:47 +0900

    feat: implement get_last_block in ColonyChain

--- colony-chain/src/lib.rs
+++ colony-chain/src/lib.rs
@@ -2,11 +2,15 @@ use async_trait::async_trait;
 use pdao_beacon_chain_common::message as pbc_message;
 use pdao_colony_common::*;
 use pdao_colony_contract_common::*;
+use pdao_polkadot_interact::{self, get_block, get_current_height};
 use rust_decimal::prelude::*;
 use rust_decimal_macros::dec;
 use std::collections::HashMap;
 
-pub struct Shiden {}
+pub struct Shiden {
+    pub full_node_uri: String,
+    pub http_server_url: String,
+}
 
 #[async_trait]
 impl ColonyChain for Shiden {
@@ -15,10 +19,15 @@ impl ColonyChain for Shiden {
     }
 
     async fn get_last_block(&self) -> Result<Block, Error> {
-        Ok(Block {
-            height: 0,
-            timestamp: 0,
-        })
+        let height = get_current_height(&self.full_node_uri, &self.http_server_url)
+            .await
+            .unwrap()
+            .unwrap();
+        let timestamp = get_block(&self.full_node_uri, &self.http_server_url, height)
+            .await
+            .unwrap()
+            .timestamp;
+        Ok(Block { height, timestamp })
     }
 
     async fn check_connection(&self) -> Result<(), Error> {
